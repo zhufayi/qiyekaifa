89ff16a4532bfaab36362fd3c6073fcf9c7a17a9
zhufayi <zhufayi@126.com>
2019-04-30 14:48:43 +0800

    学员管理系统初级，学习Form的用法

---
+++ student/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .forms import StudentForm
+from .models import Student
+# Create your views here.
+
+def index(request):
+    students = Student.get_all()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = StudentForm()
+
+    context = {
+        'students': students,
+        'form': form,
+    }
+    return render(request, 'index.html', context=context)
